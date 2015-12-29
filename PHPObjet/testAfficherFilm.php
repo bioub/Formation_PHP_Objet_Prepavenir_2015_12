bace8ef546d96cd87cd539f515641fbe3cb879b4
bioub <romain.bohdanowicz@gmail.com>
2015-12-29 11:35:28 +0100

    Enoncé film

---
+++ PHPObjet/testAfficherFilm.php
@@ -0,0 +1,25 @@
+<?php
+
+/* 
+ * Exercice : Créer un page film du type :
+ * http://www.allocine.fr/film/fichefilm_gen_cfilm=215097.html
+ * 
+ * 1 - Commencer en HTML (même très basique) 
+ * à faire la maquette avec du faux-texte
+ * 
+ * 2 - Créer les classes
+ * 
+ * 3 classes :
+ * - Film (titre, année, nom du réalisateur)
+ * - Acteur (prenom, nom)
+ * - Critique (journal, note, texte)
+ * 
+ * Uniquement dans Film ajouter le lien
+ * vers Acteur (réfléchir aux cardinalité)
+ * et Critique
+ * 
+ * 3 - Instancier les classes (créer les objets)
+ * avec des exemples et remplacer le faux-texte
+ * par les appels aux getters
+ */
+
