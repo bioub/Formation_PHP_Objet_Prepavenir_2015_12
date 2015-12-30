6b871dbbdd9a9f12bed3f1f7d22eba3ca8efbfa7
bioub <romain.bohdanowicz@gmail.com>
2015-12-30 09:25:38 +0100

    Ajout de l'exercice OffreEmploi

---
+++ PHPObjet/exOffreEmploi.php
@@ -0,0 +1,22 @@
+<?php
+/*
+ * Exercice :
+ * Créer la classe OffreEmploi (titre, datePublication, annonce)
+ * Lier la classe OffreEmploi à la classe Société (côté OffreEmploi)
+ * Créer la classe Candidat (prenom, nom)
+ * Lier la classe OffreEmploi à la classe Candidat (côté OffreEmploi)
+ */
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Offres emploi</title>
+    </head>
+    <body>
+        <h2>Développeur PHP H/F</h2>
+        <p>Google</p>
+        <p>22 déc. 2015</p>
+        <p>C2iS est une agence web spécialisée dans les domaines de l'e-tourisme et de l'immobilier. En tant que développeur junior, vous intégrez l'équipe back-end C2iS afin de participer aux développements des dispositifs web réalisés par l'agence. Encadré par un un Lead Developer, vous intervenez sur des projets PHP basés sur des technologies Symfony2. Vous intégrez les processus de développement et de déploiement de l'équipe de développement avec la validation du Lead Developer du projet.</p>
+    </body>
+</html>
